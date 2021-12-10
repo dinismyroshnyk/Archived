5c0766a82ab369605591162f15689290c2144316
Dinis Myroshnyk <95895082+dinismyroshnyk@users.noreply.github.com>
2021-12-10 12:19:28 +0000

    Update Exercicio_7_4.c

---
+++ Ex_7/Exercicio_7_4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+//@cikey 67361819476f1ac99dde38b453a6db17
+//@sid 2021157297
+//@aid 7.4
+int main(){
+
+	//begin_inputs
+
+  int meuVector[] = {1,2,3,4,5,6};
+  const int tamanho = 6;
+
+	//end_inputs
+
+
+
+	return 0;
+}
