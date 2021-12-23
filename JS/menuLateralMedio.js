4ae0d9f0f88cf5feef2756694cc709f0fb2c9854
Dinis Myroshnyk <95895082+dinismyroshnyk@users.noreply.github.com>
2021-12-23 11:53:41 +0000

    update

--- JS/menuLateralMedio.js
+++ JS/menuLateralMedio.js
@@ -1,6 +1,23 @@
 let menu = document.querySelector(".menu");
 let menu_lateral = document.querySelector(".menu_lateral");
+let menu2 = document.getElementById("menu2");
+let menu3 = document.getElementById("menu3");
+let menu4 = document.getElementById("menu4");
+let menu5 = document.getElementById("menu5");
 
 menu.onclick = function(){
+    menu_lateral.classList.toggle("active");
+}
+
+menu2.onclick = function(){
+    menu_lateral.classList.toggle("active");
+}
+menu3.onclick = function(){
+    menu_lateral.classList.toggle("active");
+}
+menu4.onclick = function(){
+    menu_lateral.classList.toggle("active");
+}
+menu5.onclick = function(){
     menu_lateral.classList.toggle("active");
 }
