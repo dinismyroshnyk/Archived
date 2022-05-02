a2ab730de693289123a1f643e3255fa388996126
Dinis Myroshnyk <95895082+dinismyroshnyk@users.noreply.github.com>
2022-05-02 01:53:11 +0000

    adding stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,41 @@
 #include <Arduino.h>
 
+/*
+ Name:		tp1.ino
+ Created:	4/29/2022 11:55:22 AM
+ Author:	dinis
+*/
+
+int estadoBotao = 0;
+
+// the setup function runs once when you press reset or power the board
 void setup() {
-  // put your setup code here, to run once:
+	
+	pinMode(2, INPUT);
+	pinMode(A0, INPUT);
+	for (int i = 3; i < 14; i++)
+	{
+		pinMode(i, OUTPUT);
+	}
+	digitalWrite(13, HIGH);
+
+	Serial.begin(9600);
 }
 
+// the loop function runs over and over again until power down or reset
 void loop() {
-  // put your main code here, to run repeatedly:
+		
+	estadoBotao = digitalRead(2);
+	
+	if (estadoBotao == HIGH)
+	{
+		Serial.println("botao on");
+		delay(2000);
+		digitalWrite(13, LOW);
+		digitalWrite(12, HIGH);
+		delay(2000);
+		digitalWrite(12, LOW);
+		digitalWrite(11, HIGH);
+		delay(2000);
+	}
 }
