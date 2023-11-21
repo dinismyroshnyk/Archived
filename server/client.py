e95dbed99e19dbc4fdab6cfab8b70c8fd40d4908
Fiugas <95915531+Fiugas@users.noreply.github.com>
2023-11-21 15:57:32 +0000

    Refactored client.py and server.py

--- server/client.py
+++ server/client.py
@@ -1,18 +1,33 @@
 import socket
 
 class Client:
-    def __init__(self, server):
-        self.server = server
+    def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client.connect((self.server.host, self.server.port))
+        self.server = '192.168.3.61'
+        self.port = 5555
+        self.addr = (self.server, self.port)
+        self.pos = self.connect()
+
+    def get_pos(self):
+        return self.pos
+
 
     def connect(self):
-        pass
-
-    def disconnect(self):
-        pass
+        try:
+            self.client.connect(self.addr)
+            return self.client.recv(2048).decode()
+        except:
+            pass
 
     def send(self, data):
-        pass
+        try:
+            self.client.send(str.encode(data))
+            return self.client.recv(2048).decode()
+        except socket.error as e:
+            print(e)
+
+    #def disconnect(self):
+       # pass
+
 
 # Not sure if anything here works at all
--- server/server.py
+++ server/server.py
@@ -1,27 +1,45 @@
-import socket, threading
+import socket
+from _thread import *
+import sys
 
-class Server:
-    def __init__(self):
-        self.host = socket.gethostname()
-        self.port = 5555
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.bind((self.host, self.port))
-        self.server.listen()
+server = '192.168.3.61'
+port = 5555
 
-    def start(self):
-        print(f"Server started on {self.host}:{self.port}")
-        while True:
-            client, address = self.server.accept()
-            print(f"Connected to {address}")
-            thread = threading.Thread(target=self.handle_client, args=(client,))
-            thread.start()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def handle_client(self, client):
-        while True:
-            data = client.recv(1024).decode('utf-8')
-            if not data: break
-            print(f"Received: {data}")
-            client.send(data.encode('utf-8'))
-        client.close()
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
 
-# Not sure if anything here works at all
+s.listen(4)
+print("Waiting for a connection, Server Started")
+
+
+def threaded_client(conn):
+    conn.send(str.encode("Connected"))
+    reply = ""
+    while True:
+        try:
+            data = conn.recv(2048)
+            reply = data.decode("utf-8")
+
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                print("Received: ", reply)
+                print("Sending: ", reply)
+
+            conn.sendall(str.encode(reply))
+        except:
+            break
+    
+    print("Lost connection")
+    conn.close()
+
+while True:
+    conn, addr = s.accept()
+    print("Connected to:", addr)
+
+    start_new_thread(threaded_client, (conn,))
