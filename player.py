937eb858156031ec7a04b3fa5937e9f90d5091a6
Dinis Myroshnyk <dinismyroshnyk2@gmail.com>
2023-12-10 15:41:52 +0000

    changed player sprites.

--- player.py
+++ player.py
@@ -50,63 +50,48 @@ class Player():
     def animate(self, dt, direction_x, direction_y):
         self.last_frame_update += dt
         if not (direction_x or direction_y):
-            self.curr_sprite = self.curr_sprite_list[0]
+            self.curr_sprite = self.sprites['down'][0]
             return
-        self.curr_sprite_list = self.get_sprite_list(direction_x, direction_y)
+        direction = self.get_direction(direction_x, direction_y)
+        self.curr_sprite_list = self.sprites[direction]
         if self.last_frame_update > .1:
             self.last_frame_update = 0
             self.curr_frame = (self.curr_frame + 1) % len(self.curr_sprite_list)
             self.curr_sprite = self.curr_sprite_list[self.curr_frame]
 
-    def get_sprite_list(self, direction_x, direction_y):
-        if direction_x and direction_y: return self.get_diagonal_sprite_list(direction_x, direction_y)
-        if direction_x: return self.get_horizontal_sprite_list(direction_x)
-        if direction_y: return self.get_vertical_sprite_list(direction_y)
-        return self.curr_sprite_list
-    
-    def get_diagonal_sprite_list(self, direction_x, direction_y):
-        sprite_map = {
-            (1, 1): self.down_right_sprites,
-            (-1, 1): self.down_left_sprites,
-            (1, -1): self.up_right_sprites,
-            (-1, -1): self.up_left_sprites,
-        }
-        return sprite_map.get((direction_x, direction_y))
-
-    def get_horizontal_sprite_list(self, direction_x):
-        sprite_map = {
-            1: self.right_sprites,
-            -1: self.left_sprites,
-        }
-        return sprite_map.get(direction_x)
-
-    def get_vertical_sprite_list(self, direction_y):
-        sprite_map = {
-            1: self.down_sprites,
-            -1: self.up_sprites,
-        }
-        return sprite_map.get(direction_y)
+    def get_direction(self, direction_x, direction_y):
+        if direction_x == 1 and direction_y == 0:
+            return 'right'
+        elif direction_x == -1 and direction_y == 0:
+            return 'left'
+        elif direction_x == 0 and direction_y == 1:
+            return 'down'
+        elif direction_x == 0 and direction_y == -1:
+            return 'up'
+        elif direction_x == 1 and direction_y == 1:
+            return 'down_right'
+        elif direction_x == -1 and direction_y == 1:
+            return 'down_left'
+        elif direction_x == 1 and direction_y == -1:
+            return 'up_right'
+        elif direction_x == -1 and direction_y == -1:
+            return 'up_left'
+        else: return 'down'
 
     def load_sprites(self):
-        self.sprites_dir = os.path.join(self.game.sprites_dir, 'entities', 'player', 'Base Character')
-        self.up_sprites, self.down_sprites, self.left_sprites, self.right_sprites = [], [], [], []
-        self.up_right_sprites, self.down_right_sprites, self.up_left_sprites, self.down_left_sprites = [], [], [], []
-        sprite_lists = {
-            'player_up_': self.up_sprites,
-            'player_down_': self.down_sprites,
-            'player_left_': self.left_sprites,
-            'player_right_': self.right_sprites,
-            'player_up_left_': self.up_left_sprites,
-            'player_up_right_': self.up_right_sprites,
-            'player_down_left_': self.down_left_sprites,
-            'player_down_right_': self.down_right_sprites
-        }
-        for sprite_name, sprite_list in sprite_lists.items():
-            for i in range(1, 4):
-                sprite = pygame.image.load(os.path.join(self.sprites_dir, sprite_name + str(i) + '.png')).convert_alpha()
-                sprite_list.append(sprite)
-        self.curr_sprite = self.down_sprites[0]
-        self.curr_sprite_list = self.down_sprites
+        self.spritesheet = pygame.image.load(os.path.join(self.game.sprites_dir, 'entities', 'player', 'Knight1_Move.png')).convert_alpha()
+        self.frame_width = 52  # width of each frame in pixels
+        self.frame_height = 52  # height of each frame in pixels
+        self.scaled_width = 128  # new width after scaling
+        self.scaled_height = 128  # new height after scaling
+        self.directions = ['down', 'down_right', 'right', 'up_right', 'up', 'up_left', 'left', 'down_left']
+        self.sprites = {direction: [] for direction in self.directions}
+        for i, direction in enumerate(self.directions):
+            for j in range(4):  # 4 sprites per direction
+                frame = self.spritesheet.subsurface(pygame.Rect(j * self.frame_width, i * self.frame_height, self.frame_width, self.frame_height))
+                scaled_frame = pygame.transform.scale(frame, (self.scaled_width, self.scaled_height))
+                self.sprites[direction].append(scaled_frame)
+        self.curr_sprite = self.sprites['down'][0]
 
     def get_client_id(self):
         for char in self.chars:
